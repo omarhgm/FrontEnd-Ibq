9deadfdec55bc7d2e190269848bccb2facb5c263
OmarGM <omarhgm73@gmail.com>
2019-09-27 16:25:32 -0500

    Creacion del modelo Guante

---
+++ src/app/models/guante.spec.ts
@@ -0,0 +1,7 @@
+import { Guante } from './guante';
+
+describe('Guante', () => {
+  it('should create an instance', () => {
+    expect(new Guante()).toBeTruthy();
+  });
+});
---
+++ src/app/models/guante.ts
@@ -0,0 +1,14 @@
+export class Guante {
+  id_Guante: number;
+  nombre_Guante: String;
+  url_Image: String;
+  descripcion_Guante: String;
+  color_Guante: String;
+  textura_Guante: String;
+  espesor_Guante: String;
+  elogacion_Guante: String;
+  longitud_Guante: String;
+  contenido_Guante: String;
+  tallas_Guante: String;
+  detalles_Guante: String;
+}
