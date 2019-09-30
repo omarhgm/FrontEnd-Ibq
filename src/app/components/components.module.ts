ebef73f8f90a13cb6590bc51bb3e89f5d76430a5
OmarGM <omarhgm73@gmail.com>
2019-09-30 11:18:23 -0500

    Navbar component y responsive

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -4,10 +4,16 @@ import { CommonModule } from "@angular/common";
 import { NavbarComponent } from "./navbar/navbar.component";
 import { PruevaComponent } from "./prueva/prueva.component";
 import { Prueba2Component } from "./prueba2/prueba2.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @NgModule({
-  declarations: [NavbarComponent, PruevaComponent, Prueba2Component],
+  declarations: [
+    NavbarComponent,
+    PruevaComponent,
+    Prueba2Component,
+    FooterComponent
+  ],
   imports: [CommonModule, RouterModule],
-  exports: [NavbarComponent, PruevaComponent, Prueba2Component]
+  exports: [NavbarComponent, PruevaComponent, Prueba2Component, FooterComponent]
 })
 export class ComponentsModule {}
